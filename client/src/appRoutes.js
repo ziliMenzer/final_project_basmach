367edb073cc7b0ac97302b2f666f0bf4d1f0336c
tamar329 <tamar7475@gmail.com>
2023-06-25 15:10:07 +0300

    Add register

--- client/src/appRoutes.js
+++ client/src/appRoutes.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import { AppContext } from "../src/context/userProvider"
 import Login from './general_comps/login'
+import SignUp from './student_comps/register'
 import Home from './general_comps/home'
 
 export default function AppRoutes() {
@@ -14,17 +15,17 @@ export default function AppRoutes() {
             }}>
 
                 <header className='p-2 container bg-warning'>
-                    <Link to="/">Home</Link> 
-                    <Link to="/login">login</Link> 
-                    <Link to="/counter">Counter</Link> 
-                    <Link to="/pixa/cats">Pixa</Link> 
-                    <Link to="/cars">Cars</Link> 
-                    <Link to="/casino">Casino</Link>
+                    <Link to="/">Home </Link> 
+                    <Link to="/login">login </Link> 
+                    <Link to="/register">register </Link> 
+                    <Link to="/counter">Counter </Link> 
+                    <Link to="/pixa/cats">Pixa </Link> 
                 </header>
                 {/* outlet */}
                 <Routes>
                     <Route index element={<Home />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<SignUp />} />
                     {/* <Route path="/counter" element={<Counter />} />
                     <Route path="/pixa/:searchQ" element={<AppPixa />} />
                     <Route path="/casino" element={<AppCasino />} />
