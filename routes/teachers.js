9506d9c03ef51a664956ffaf2f606dcd56581288
Dvora Mizrachi <dvorry.mm@gmail.com>
2023-06-25 14:17:55 +0300

    .

--- routes/teachers.js
+++ routes/teachers.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 router.get("/", async (req, res) => {
-  res.json({ msg: "Rest api work" })
+  res.json({ msg: "Rest api work " })
 });
 module.exports = router;
