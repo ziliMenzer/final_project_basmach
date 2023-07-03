971010583cbd120f9a567046e767ca8b6fc82ad6
TZILI M <zilimenzer@gmail.com>
2023-07-03 09:19:34 +0300

    animation works

--- client/src/student_comps/progress.js
+++ client/src/student_comps/progress.js
@@ -2,20 +2,39 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/userProvider';
 import "./progress.scss";
 import { keyframes } from "styled-components";
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
 
 const Progress = () => {
   const { user } = useContext(AppContext);
   const subjectsArray = user.subjects_array;
+  let total = 0;
+  Object.keys(subjectsArray).map(item => {
+    total += parseInt(subjectsArray[item] * 10);
+  });
+  total = Math.round(total / 6);
+  console.log(total)
   const subjects_array_hebrew = ["תמרורים", "פניות", "מהירות", "תפעול הרכב", "דרך בין-עירונית", "זכויות קדימה"]
 
   return (
     <div className='container'>
+      <h2>total: {total}%</h2>
+      <ProgressBar>
+        <ProgressBar now={total}  label={`${total}%`} style={{
+          backgroundColor: '#EF476F',
+          animation: `load 6s`,
+        }} />
+        {/* <ProgressBar now={(100 - total)} key={2} style={{ backgroundColor: "grey" }} /> */}
+      </ProgressBar>
+      <br />
+      {/* <ProgressBar variant='danger' now={total} label={`${total}%`} /> */}
       <div className='smaller-container'>
         {Object.keys(subjectsArray).map((subject, index) => (
           <div key={index} className='m-4'>
+
             <h3>{subjects_array_hebrew[index]}:</h3>
-            <div class="progress2 progress-moved">
-              <div class="progress-bar2" style={{
+            <div className="progress2 progress-moved">
+              <div className="progress-bar2" style={{
                 width: `${subjectsArray[subject] * 10}%`,
                 backgroundColor: '#EF476F',
                 animation: `load 6s`,
